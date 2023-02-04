20202a424bc911fc33d765f74d78e27f6d46d1ab
Unni Ramachandran <unnipbvr@gmail.com>
2023-02-04 10:52:18 -0500

    Sending messages to queue

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rabbitmq-sender
 Node JS app sending message to rabbitmq
+
+# References:
+https://sharmilas.medium.com/get-started-with-rabbitmq-in-node-js-1adb18d019d0
+
+# Install and Start Rabbit MQ
+https://www.rabbitmq.com/install-homebrew.html
+
+# GUI for Rabbit MQ
+http://localhost:15672/
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const amqp = require("amqplib");
+
+var channel, connection; //global variables
+async function connectQueue() {
+  try {
+    connection = await amqp.connect("amqp://127.0.0.1:5672");
+    channel = await connection.createChannel();
+
+    await channel.assertQueue("test-queue");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function sendData(data) {
+  await connectQueue();
+  // send data to queue
+  await channel.sendToQueue("test-queue", Buffer.from(JSON.stringify(data)));
+
+  // close the channel and connection
+  await channel.close();
+  await connection.close();
+}
+const app = express();
+const PORT = process.env.PORT || 4001;
+app.use(express.json());
+app.get("/send-msg", (req, res) => {
+  // data to be sent
+  const data = {
+    title: "Six of Crows",
+    author: "Leigh Burdugo",
+  };
+  sendData(data); // pass the data to the function we defined
+  console.log(" messages sent to queue");
+  res.send("Message Sent"); //response to the API request
+});
+app.listen(PORT, () => console.log("Server running at port " + PORT));
